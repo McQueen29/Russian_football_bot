c044545146b2dd14f5ee5de06502ef9d3bdca486
s-eugene-v <95971762+s-eugene-v@users.noreply.github.com>
2022-04-25 19:58:39 +0300

    Add files via upload

--- russian_futbol_bot.py
+++ russian_futbol_bot.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 import logging
-import csv
+import sqlite3
 from telegram.ext import MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackContext
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 
 logger = logging.getLogger(__name__)
-reply_keyboard = [['/news', '/statistic'],
-                  ['/developers', '/info'],
-                  ['/social_media']]
+reply_keyboard = [['Новости', 'Статистика'],
+                  ['Разработчикам', 'Информация'],
+                  ['------']]
 
 TOKEN = '5193054775:AAHmmNiMl5903TX_C8Wk9Xp6fJ2REQVvdyE'
 
@@ -39,7 +39,7 @@ def close_keyboard(bot, context):
 
 def statistic(bot, context):
     close_keyboard(bot, context)
-    keyboard_stat = [['Турнирная талица', 'Бомбардиры'],
+    keyboard_stat = [['Турнирная таблица', 'Бомбардиры'],
                      ['"Сухие" матчи', 'Голевые передачи'],
                      ["Желыте карточки", "Красные карточки"],
                      ["Назад"]]
@@ -65,7 +65,9 @@ def news(bot, context):
 
 
 def developers(bot, context):
-    bot.message.reply_text("will be soon")
+    bot.message.reply_text("Хочешь оставить для нас послание/пожелание/предложение/похвалу? \n"
+                           "Сеня - https://t.me/molnia_macvin \n"
+                           "Или напиши: <Обратная связь> ")
 
 
 def social_media(bot, context):
@@ -107,23 +109,51 @@ def info(bot, context):
         "will be soon")
 
 
+def vopros1(bot, context):
+    bot.message.reply_text(f'Пишите вопрос')
+    return 1
+
+
+def accept_vopros(bot, context):
+    id = bot.message.chat.id
+    name = bot.message.chat.first_name
+    otvet = bot.message.text
+    con = sqlite3.connect('vopros.db')
+    cur = con.cursor()
+    result = cur.execute("""INSERT INTO vopros(id, name, vopros) VALUES(?, ?, ?)""",
+                         [id, name, otvet])
+    con.commit()
+    con.close()
+    bot.message.reply_text(f"Вопрос принят")
+    return ConversationHandler.END
+
+
 def main():
     my_bot = Updater(TOKEN)
     dp = my_bot.dispatcher
     text_handler = MessageHandler(Filters.text & ~Filters.command, start)
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("info", info))
-    dp.add_handler(CommandHandler("news", news))
-    dp.add_handler(MessageHandler(Filters.regex("Турнирная талица"), table_rpl))
+    dp.add_handler(MessageHandler(Filters.regex("Статистика"), statistic))
+    dp.add_handler(MessageHandler(Filters.regex("Новости"), news))
+    dp.add_handler(MessageHandler(Filters.regex("Турнирная таблица"), table_rpl))
     dp.add_handler(MessageHandler(Filters.regex("Бомбардиры"), goals))
     dp.add_handler(MessageHandler(Filters.regex("Голевые передачи"), assists))
     dp.add_handler(MessageHandler(Filters.regex('"Сухие" матчи'), zeros))
     dp.add_handler(MessageHandler(Filters.regex('Желыте карточки'), yk))
     dp.add_handler(MessageHandler(Filters.regex('Красные карточки'), kk))
     dp.add_handler(MessageHandler(Filters.regex("Назад"), start))
-    dp.add_handler(CommandHandler("developers", developers))
+    dp.add_handler(MessageHandler(Filters.regex("Разработчикам"), developers))
     dp.add_handler(CommandHandler("statistic", statistic))
     dp.add_handler(CommandHandler("social_media", social_media))
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.regex("Обратная связь"), vopros1)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, accept_vopros)]
+        },
+        fallbacks=[CommandHandler('ok', start)]
+    )
+
+    dp.add_handler(conv_handler)
     dp.add_handler(text_handler)
     my_bot.start_polling()
 
